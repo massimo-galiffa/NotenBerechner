f53adda90d09af784a972d25f5a3e691be8afc69
Massimo Galiffa <galiffa@pentag.ch>
2025-04-03 09:22:30 +0200

    added adsense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,29 @@
 import type React from "react"
-import { Inter } from "next/font/google"
-import { ThemeProvider } from "@/components/theme-provider"
+import {Inter} from "next/font/google"
+import {ThemeProvider} from "@/components/theme-provider"
 import "./globals.css"
 
-const inter = Inter({ subsets: ["latin"] })
+const inter = Inter({subsets: ["latin"]})
 
 export const metadata = {
-  title: "Noten Berechner",
-  description: "Calculate grades based on different country systems",
+    title: "Noten Berechner",
+    description: "Calculate grades based on different country systems",
 }
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+    return (
+        <html lang="en" suppressHydrationWarning>
+        <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-          {children}
+            {children}
+            <meta name="google-adsense-account" content="ca-pub-4084881745214312"/>
         </ThemeProvider>
-      </body>
-    </html>
-  )
+        </body>
+        </html>
+    )
 }
 
